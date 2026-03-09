c4bd706b36fe44bfce749a177158908551170af0
Syed Nouman Hasany <127871419+for-submission-2023@users.noreply.github.com>
2026-03-09 11:44:03 +0100

    Remove tqdm from dataset processing loop
    Removed tqdm progress bar from dataset iteration.

--- utils/metrics.py
+++ utils/metrics.py
@@ -16,7 +16,7 @@ def evaluate(dataset, model, num_classes, device='cpu', label_dict=None):
     # classes with no appearances still show up in the per-class printout
     result_dict = {cls: [] for cls in range(1, num_classes)}
     
-    for i in tqdm(range(len(dataset))):
+    for i in range(len(dataset)):
         image, label = dataset[i]
         image, label = image.to(device), label.numpy()
         
